107fdd473b9fed99971ea425d528c4ecb0b8c550
bcheeseman18 <benjamincheeseman18@gmail.com>
2017-10-12 12:29:36 -0400

    added jQuery functionality

--- todolist.js
+++ todolist.js
@@ -1,16 +1,27 @@
 window.addEventListener('load', function load(event){
 //check off to dos by clicking
-    $('li').click(function(){
+    $('ul').on('click', 'li', function(){
         $(this).toggleClass('completed'); 
     }); 
 
 //click on x to delete todo
 
-    $('span').click(function(event){
+    $('ul').on('click', 'span', function(event){
         $(this).parent().fadeOut(500, function(){
             $(this).remove(); 
         }); 
         event.stopPropagation(); 
     }); 
+
+    $("input[type='text']").keypress(function (event){
+        if(event.which === 13) {
+            //grabbing new text from input
+            let toDoText = $(this).val();  
+            $(this).val(""); 
+            //create new li and add to ul
+            $('ul').append("<li><span>X</span> " + toDoText + "</li>"); 
+        }
+    }); 
+
 
 }); 
